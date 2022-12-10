9e95091579d2dd4e63beef67b07e1281d59136b5
JohanLeon0914 <johanalbertols@ufps.edu.co>
2022-12-10 09:50:42 -0500

    agregando dist

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB6HfPw3rknJwM8-oeGhfKJN6E_FMXyRqk",
+    authDomain: "crud-angular-firebase-5c874.firebaseapp.com",
+    projectId: "crud-angular-firebase-5c874",
+    storageBucket: "crud-angular-firebase-5c874.appspot.com",
+    messagingSenderId: "222000061642",
+    appId: "1:222000061642:web:210eee559cca87235a2833"
+  }
 };
